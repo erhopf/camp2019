2ac5305f3cb8076a745a5654b65275bb3968e8f9
erhopf <erhopf@microsoft.com>
2019-01-30 17:14:40 -0800

    Updating for workshop.

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from flask import Flask, render_template, url_for, jsonify, request
-import translate, synthesize, sentiment, sys
-
-app = Flask(__name__)
-app.config['JSON_AS_ASCII'] = False
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+# This is your Flask app
