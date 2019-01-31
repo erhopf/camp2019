bc8b7a0a04a80542be9870e16403311ba656ccb3
erhopf <erhopf@microsoft.com>
2019-01-31 13:25:05 -0800

    Source code.

--- sentiment.py
+++ sentiment.py
@@ -1,1 +1,42 @@
-# Copy and paste the code from the workshop below this line.
+import os, requests, uuid, json
+
+if 'SENTIMENT_ANALYSIS_KEY' in os.environ:
+    subscription_key = os.environ['SENTIMENT_ANALYSIS_KEY']
+else:
+    print('Environment variable for SENTIMENT_ANALYSIS_KEY is not set.')
+    exit()
+
+# If you're not comfortable using environment variables, you can hardcode
+# your subscription key on the next line. Don't forget to uncomment this line,
+# and uncomment lines 3-7.
+
+#subscription_key="YOUR_KEY"
+
+def get_sentiment(input_text, input_language, output_text, output_language):
+    base_url = 'https://westus.api.cognitive.microsoft.com/text/analytics'
+    path = '/v2.0/sentiment'
+    constructed_url = base_url + path
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = {
+        "documents": [
+            {
+                "language": input_language,
+                "id": "1",
+                "text": input_text
+            },
+            {
+                "language": output_language,
+                "id": "2",
+                "text": output_text
+            }
+        ]
+    }
+    response = requests.post(constructed_url, headers=headers, json=body)
+    return response.json()
--- translate.py
+++ translate.py
@@ -1,1 +1,32 @@
-# Copy and paste the code from the workshop below this line.
+import os, requests, uuid, json
+
+if 'TRANSLATOR_TEXT_KEY' in os.environ:
+    subscription_key = os.environ['TRANSLATOR_TEXT_KEY']
+else:
+    print('Environment variable for TRANSLATOR_TEXT_KEY is not set.')
+    exit()
+
+# If you're not comfortable using environment variables, you can hardcode
+# your subscription key on the next line. Don't forget to uncomment this line,
+# and uncomment lines 3-7.
+
+#subscription_key="YOUR_KEY"
+
+def get_translation(text_input, language_output):
+    base_url = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    params = '&to=' + language_output
+    constructed_url = base_url + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        "text" : text_input
+    }]
+    response = requests.post(constructed_url, headers=headers, json=body)
+    return response.json()
